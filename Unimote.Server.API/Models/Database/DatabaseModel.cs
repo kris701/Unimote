d0462680a5cc2b1ff0cf70aa43266016fdf74001
Kristian Skov <kris701kj@gmail.com>
2024-08-04 12:23:39 +0200

    Database and settings saving fix

--- Unimote.Server.API/Models/Database/DatabaseModel.cs
+++ Unimote.Server.API/Models/Database/DatabaseModel.cs
@@ -4,17 +4,21 @@ using Unimote.Server.API.Models.Users;
 
 namespace Unimote.Server.API.Models.Database
 {
-	public class DatabaseModel : SavableModel
+	public class DatabaseModel
 	{
+		public string FileName { get; set; }
 		public List<UserModel> Users { get; set; }
 		public List<RemoteConfiguration> RemoteConfigurations { get; set; }
 
-		public DatabaseModel(string fileName) : base (fileName)
+		public DatabaseModel(string fileName)
 		{
+			FileName = fileName;
 			Users = new List<UserModel>() {
 				new UserModel("admin", "password")
 			};
 			RemoteConfigurations = new List<RemoteConfiguration>();
 		}
+
+		public void Save() => File.WriteAllText(FileName, JsonSerializer.Serialize(this));
 	}
 }
--- Unimote.Server.API/Models/Settings/SettingsModel.cs
+++ Unimote.Server.API/Models/Settings/SettingsModel.cs
@@ -1,18 +1,24 @@
-﻿namespace Unimote.Server.API.Models.Settings
+﻿using System.Text.Json;
+
+namespace Unimote.Server.API.Models.Settings
 {
-	public class SettingsModel : SavableModel
+	public class SettingsModel
 	{
+		public string FileName { get; set; }
 		public string JWTSecret { get; set; }
 		public int JWTTokenLifetime { get; set; }
 		public bool EnableDirectControl { get; set; }
 		public bool EnableWebControl { get; set; }
 
-		public SettingsModel(string fileName) : base(fileName)
+		public SettingsModel(string fileName)
 		{
+			FileName = fileName;
 			JWTSecret = "some secretsome secretsome secretsome secretsome secretsome secretsome secretsome secret";
 			JWTTokenLifetime = 10000;
 			EnableDirectControl = false;
 			EnableWebControl = false;
 		}
+
+		public void Save() => File.WriteAllText(FileName, JsonSerializer.Serialize(this));
 	}
 }
